433dfafd539f55d749fa895b3df55412abeb0777
john-wiens <jwiens3141@gmail.com>
2022-07-07 23:06:59 -0600

    Connect ESP32 to wifi and show IP

---
+++ MIRV-Garage/src/network.h
@@ -0,0 +1,2 @@
+const char* ssid = "";         // The SSID (name) of the Wi-Fi network you want to connect to
+const char* password = "";       // The password of the Wifi network you want to connect to
