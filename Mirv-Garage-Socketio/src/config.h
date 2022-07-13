7f9cf22d4147b65a9b495f282531ae1d7c00e1d4
john-wiens <jwiens3141@gmail.com>
2022-07-12 18:42:25 -0600

    ESP32 connects to API via websockets

---
+++ Mirv-Garage-Socketio/src/config.h
@@ -0,0 +1,5 @@
+const char* ssid = "";         // The SSID (name) of the Wi-Fi network you want to connect to
+const char* password = "";       // The password of the Wifi network you want to connect to
+const String apiHost = "";
+const int apiPort = 0;
+const String garageID = "";
