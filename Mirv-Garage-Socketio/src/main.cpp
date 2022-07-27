f5f768c531e820be04ad83bc8c368932d6ef8293
john-wiens <jwiens@neaeraconsulting.com>
2022-07-27 16:55:23 -0600

    Integrated RoboClaw Motor Controller

--- Mirv-Garage-Socketio/src/main.cpp
+++ Mirv-Garage-Socketio/src/main.cpp
@@ -1,4 +1,3 @@
-
 #include <Arduino.h>
 #include <WiFi.h>
 #include <WiFiMulti.h>
@@ -8,120 +7,128 @@
 #include <SocketIOclient.h>
 #include <HTTPClient.h>
 #include <U8x8lib.h>
+#include <RoboClaw.h>
+
 #include "config.h"
 
 #define USE_SERIAL Serial
+#define robotClawAddress 0x80
 
-
+RoboClaw roboclaw(&Serial2, 10000);
 
 WiFiMulti WiFiMulti;
 SocketIOclient socketIO;
-U8X8_SSD1306_128X64_NONAME_SW_I2C u8x8(/* clock=*/ 15, /* data=*/ 4, /* reset=*/ 16);
+U8X8_SSD1306_128X64_NONAME_SW_I2C u8x8(/* clock=*/15, /* data=*/4, /* reset=*/16);
 String token = "";
 
-
-void drawStatus(){
+void drawStatus()
+{
 
     IPAddress address = WiFi.localIP();
     String addressString = address.toString();
-
-
 
     u8x8.drawString(0, 0, "Connected To:");
     u8x8.drawString(0, 1, ssid);
     u8x8.drawString(0, 3, "Current IP: ");
     u8x8.drawString(0, 4, addressString.c_str());
-    if(token != ""){
+    if (token != "")
+    {
         u8x8.drawString(0, 6, "Token: Acquired");
-    } else{
+    }
+    else
+    {
         u8x8.drawString(0, 6, "Token: Missing");
     }
-  
-
-
 }
 
-void socketIOEvent(socketIOmessageType_t type, uint8_t * payload, size_t length) {
-    switch(type) {
-        case sIOtype_DISCONNECT:
-            USE_SERIAL.printf("[IOc] Disconnected!\n");
-            break; 
-        case sIOtype_CONNECT:
-            USE_SERIAL.printf("[IOc] Connected to url: %s\n", payload);
+void socketIOEvent(socketIOmessageType_t type, uint8_t *payload, size_t length)
+{
+    switch (type)
+    {
+    case sIOtype_DISCONNECT:
+        USE_SERIAL.printf("[IOc] Disconnected!\n");
+        break;
+    case sIOtype_CONNECT:
+        USE_SERIAL.printf("[IOc] Connected to url: %s\n", payload);
 
-            // join default namespace (no auto join in Socket.IO V3)
-            socketIO.send(sIOtype_CONNECT, "/");
-            break;
-        case sIOtype_EVENT:
+        // join default namespace (no auto join in Socket.IO V3)
+        socketIO.send(sIOtype_CONNECT, "/");
+        break;
+    case sIOtype_EVENT:
+    {
+        char *sptr = NULL;
+        int id = strtol((char *)payload, &sptr, 10);
+        USE_SERIAL.printf("[IOc] get event: %s id: %d\n", payload, id);
+        if (id)
         {
-            char * sptr = NULL;
-            int id = strtol((char *)payload, &sptr, 10);
-            USE_SERIAL.printf("[IOc] get event: %s id: %d\n", payload, id);
-            if(id) {
-                payload = (uint8_t *)sptr;
-            }
-            DynamicJsonDocument doc(1024);
-            DeserializationError error = deserializeJson(doc, payload, length);
-            if(error) {
-                USE_SERIAL.print(F("deserializeJson() failed: "));
-                USE_SERIAL.println(error.c_str());
-                return;
-            }
-
-            String eventName = doc[0];
-            USE_SERIAL.printf("[IOc] event name: %s\n", eventName.c_str());
-
-            // Message Includes a ID for a ACK (callback)
-            if(id) {
-                // creat JSON message for Socket.IO (ack)
-                DynamicJsonDocument docOut(1024);
-                JsonArray array = docOut.to<JsonArray>();
-
-                // add payload (parameters) for the ack (callback function)
-                JsonObject param1 = array.createNestedObject();
-                param1["now"] = millis();
-
-                // JSON to String (serializion)
-                String output;
-                output += id;
-                serializeJson(docOut, output);
-
-                // Send event
-                socketIO.send(sIOtype_ACK, output);
-            }
+            payload = (uint8_t *)sptr;
         }
-            break;
-        case sIOtype_ACK:
-            USE_SERIAL.printf("[IOc] get ack: %u\n", length);
-            break;
-        case sIOtype_ERROR:
-            USE_SERIAL.printf("[IOc] get error: %u\n", length);
-            break;
-        case sIOtype_BINARY_EVENT:
-            USE_SERIAL.printf("[IOc] get binary: %u\n", length);
-            break;
-        case sIOtype_BINARY_ACK:
-            USE_SERIAL.printf("[IOc] get binary ack: %u\n", length);
-            break;
+        DynamicJsonDocument doc(1024);
+        DeserializationError error = deserializeJson(doc, payload, length);
+        if (error)
+        {
+            USE_SERIAL.print(F("deserializeJson() failed: "));
+            USE_SERIAL.println(error.c_str());
+            return;
+        }
+
+        String eventName = doc[0];
+        USE_SERIAL.printf("[IOc] event name: %s\n", eventName.c_str());
+
+        // Message Includes a ID for a ACK (callback)
+        if (id)
+        {
+            // creat JSON message for Socket.IO (ack)
+            DynamicJsonDocument docOut(1024);
+            JsonArray array = docOut.to<JsonArray>();
+
+            // add payload (parameters) for the ack (callback function)
+            JsonObject param1 = array.createNestedObject();
+            param1["now"] = millis();
+
+            // JSON to String (serializion)
+            String output;
+            output += id;
+            serializeJson(docOut, output);
+
+            // Send event
+            socketIO.send(sIOtype_ACK, output);
+        }
+    }
+    break;
+    case sIOtype_ACK:
+        USE_SERIAL.printf("[IOc] get ack: %u\n", length);
+        break;
+    case sIOtype_ERROR:
+        USE_SERIAL.printf("[IOc] get error: %u\n", length);
+        break;
+    case sIOtype_BINARY_EVENT:
+        USE_SERIAL.printf("[IOc] get binary: %u\n", length);
+        break;
+    case sIOtype_BINARY_ACK:
+        USE_SERIAL.printf("[IOc] get binary ack: %u\n", length);
+        break;
     }
 }
 
-void connectToNetwork(){
+void connectToNetwork()
+{
     u8x8.clearDisplay();
     u8x8.drawString(0, 2, "Connecting to:");
     u8x8.drawString(0, 4, ssid);
     int index = 0;
-    while(WiFiMulti.run() != WL_CONNECTED) {
+    while (WiFiMulti.run() != WL_CONNECTED)
+    {
         delay(100);
-        
-        
-        if(index > 16){
-            index =0;
+
+        if (index > 16)
+        {
+            index = 0;
             u8x8.clearLine(6);
         }
         u8x8.drawString(index, 6, ssid);
 
-        index +=1;
+        index += 1;
     }
     String ip = WiFi.localIP().toString();
     USE_SERIAL.printf("[SETUP] WiFi Connected %s\n", ip.c_str());
@@ -129,18 +136,20 @@ void connectToNetwork(){
     drawStatus();
 }
 
-void getToken(){
+void getToken()
+{
 
     HTTPClient http;
-    String endpoint = "http://" +apiHost + ":" + apiPort+"/token";
-    
+    String endpoint = "http://" + apiHost + ":" + apiPort + "/token";
+
     http.begin(endpoint.c_str());
     http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-    String auth = "grant_type=&username="+username+"&password="+apiPassword+"&scope=&client_id=&client_secret=";
+    String auth = "grant_type=&username=" + username + "&password=" + apiPassword + "&scope=&client_id=&client_secret=";
 
     Serial.println("Sending Request for Token");
     int httpResponseCode = http.POST(auth);
-    if (httpResponseCode>0) {
+    if (httpResponseCode > 0)
+    {
         Serial.print("HTTP Response code: ");
         Serial.println(httpResponseCode);
         String payload = http.getString();
@@ -148,20 +157,25 @@ void getToken(){
         DynamicJsonDocument doc(4096);
         DeserializationError error = deserializeJson(doc, payload);
 
-        if(error){
+        if (error)
+        {
             Serial.println("Encountered Error in JSON Parsing");
             Serial.print(error.f_str());
         }
 
-        if(doc.containsKey("access_token")){
+        if (doc.containsKey("access_token"))
+        {
             Serial.println("Found Access Token");
-            const char* access_token = doc["access_token"];
+            const char *access_token = doc["access_token"];
             token = String(access_token);
-        } else {
+        }
+        else
+        {
             Serial.println("Failed to find access token");
         }
     }
-    else {
+    else
+    {
         Serial.print("Error code: ");
         Serial.println(httpResponseCode);
     }
@@ -169,26 +183,33 @@ void getToken(){
     drawStatus();
 }
 
-void setupScreen(){
+void setupScreen()
+{
     u8x8.begin();
     u8x8.setFont(u8x8_font_chroma48medium8_r);
     u8x8.clearDisplay();
 }
 
-void setupSerial(){
+void setupSerial()
+{
     USE_SERIAL.begin(115200);
     USE_SERIAL.setDebugOutput(true);
 }
 
-void setup() {
-    
+void setup()
+{
+
     // Setup Screen for Operation
     setupScreen();
 
     // Setup Serial Bus
     setupSerial();
 
-    for(uint8_t t = 4; t > 0; t--) {
+    // Setup RoboClaw
+    roboclaw.begin(38400);
+
+    for (uint8_t t = 4; t > 0; t--)
+    {
         USE_SERIAL.printf("[SETUP] BOOT WAIT %d...\n", t);
         USE_SERIAL.flush();
         delay(1000);
@@ -200,23 +221,24 @@ void setup() {
 
     getToken();
 
-    String headers = "GARAGEID:" + garageID+"\nAuthorization:Bearer"+token;
+    String headers = "GARAGEID:" + garageID + "\nAuthorization:Bearer" + token;
 
     socketIO.setExtraHeaders(headers.c_str());
-    
+
     socketIO.begin(apiHost, apiPort, "/ws/socket.io/?EIO=4");
     socketIO.onEvent(socketIOEvent);
-    //socketIO.send(sIOtype_CONNECT, "/");
-    
+    // socketIO.send(sIOtype_CONNECT, "/");
 }
 
 unsigned long messageTimestamp = 0;
-void loop() {
+void loop()
+{
 
     socketIO.loop();
     uint64_t now = millis();
 
-    if(now - messageTimestamp > 2000) {
+    if (now - messageTimestamp > 2000)
+    {
         messageTimestamp = now;
 
         // creat JSON message for Socket.IO (event)
@@ -229,7 +251,7 @@ void loop() {
 
         // add payload (parameters) for the event
         JsonObject param1 = array.createNestedObject();
-        param1["now"] = (uint32_t) now;
+        param1["now"] = (uint32_t)now;
 
         // JSON to String (serializion)
         String output;
@@ -241,5 +263,9 @@ void loop() {
         // Print JSON for debugging
         USE_SERIAL.println(output);
     }
-    
+    //roboclaw.ForwardM1(0x80, 64);
+    //delay(2000);
+  
+    //roboclaw.ForwardM1(0x80, 0);
+    //delay(2000);
 }
